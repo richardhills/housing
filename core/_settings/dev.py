f12cd14a28540c7cc482ad95a50ef16fc8b60988
Richard Hills <richard.hills@10to8.com>
2015-03-08 11:26:02 +0000

    Bloody statics

--- core/_settings/dev.py
+++ core/_settings/dev.py
@@ -3,3 +3,11 @@ from core._settings.common import *
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = "static/"
+
--- core/_settings/production.py
+++ core/_settings/production.py
@@ -4,3 +4,10 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = 'http://richardhills.pythonanywhere.com/static/'
+
+STATIC_ROOT = "static/"
