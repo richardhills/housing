676ca465173274a88b348a1a07a1910ca0a50e1c
Richard Hills <richard.hills@10to8.com>
2015-03-08 22:39:40 +0000

    Layout improvements, adding descriptions

--- apps/home/app/scripts/controlbar.js
+++ apps/home/app/scripts/controlbar.js
@@ -13,7 +13,7 @@ module.exports = React.createClass({
 
   createCheckBox: function(value, label) {
     return (
-      <li>
+      <div>
         <input
             type="radio"
             name="type"
@@ -22,7 +22,7 @@ module.exports = React.createClass({
             id={value}
             onChange={this.buildTypeChanged} />
         <label htmlFor={value}>{label}</label>
-      </li>
+      </div>
     );
   },
 
@@ -38,14 +38,17 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="home-controlbar">
-        <h2>Double click on the map to start building</h2>
-        <p>What would you build?</p>
-        <ul>
-          {this.createCheckBox('flats', 'Flats')}
-          {this.createCheckBox('terraced', 'Terraced Homes')}
-          {this.createCheckBox('semi-detached', 'Semi-detached Homes')}
-          {this.createCheckBox('detached', 'Detached Homes')}
-        </ul>
+        <h2>The Oxford Housing Crisis</h2>
+        <p>
+          Oxford requires 28,000 homes by 2026, and Oxfordshire requires more than 100,000. There is much political disagreement over these numbers (mostly the <a href="http://www.cpreoxon.org.uk/events/current-events/item/2426-public-meeting-on-over-development-of-oxfordshire">CPRE</a> and some <a href="http://www.lgcplus.com/opinion/lgc-columnists/in-depth-special-features/why-housing-is-our-priority-in-cash-strapped-times/5075577.article">City Councillors</a>
+          ), but one thing is certain; Oxford is desperately short of housing.
+        </p>
+        <p>Where would you build?</p>
+        <p>Double click on the map to start building</p>
+        {this.createCheckBox('flats', 'Flats')}
+        {this.createCheckBox('terraced', 'Terraced Homes')}
+        {this.createCheckBox('semi-detached', 'Semi-detached Homes')}
+        {this.createCheckBox('detached', 'Detached Homes')}
         <p>Total new build:</p>
         <ul>
           <li>Flats: {this.props.map_store.getHomesBuilt().get('flats')}</li>
