032dc69f1721c2a2e348ae2360a70446d14d8364
Richard Hills <richard.hills@10to8.com>
2015-03-22 10:14:41 +0000

    Simplify key

--- apps/home/app/scripts/key.js
+++ apps/home/app/scripts/key.js
@@ -19,25 +19,9 @@ module.exports = React.createClass({
                   Trunk road
                 </td>
               </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/trunk12-b87f6ae3ed8e354b5f2460ffdaf2955e.png" alt="Trunk12"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Trunk road
-                </td>
-              </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="7" data-zoom-max="11">
                 <td className="mapkey-table-key">
                   <img src="static/assets/key_files/primary-a3c0c00bedcb07b6d91298ad6122c2e4.png" alt="Primary"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Primary road
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/primary12-35b487fd091a4010094a55e9872a892e.png" alt="Primary12"/>
                 </td>
                 <td className="mapkey-table-value">
                   Primary road
@@ -51,124 +35,12 @@ module.exports = React.createClass({
                   Secondary road
                 </td>
               </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/secondary12-c0eb60fb0ba1372c7a34c349819daa4d.png" alt="Secondary12"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Secondary road
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/unsurfaced-c1185d4dfa708993f60b5c9a4264a622.png" alt="Unsurfaced"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Unsurfaced road
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/track-062bd57595fe730ac4306d394fcbeeea.png" alt="Track"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Track
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/byway-81e1fe245217f4cb6f3563c0d06e3a23.png" alt="Byway"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Byway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/bridleway-d9c21ef9e5644bff0f000c08bec1d0e7.png" alt="Bridleway"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Bridleway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/cycleway-0053fbc37f2a218b7a73efcd2c83891c.png" alt="Cycleway"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Cycleway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/footway-33a9bb8c5003cf373733794426901704.png" alt="Footway"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Footway
-                </td>
-              </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="8" data-zoom-max="12">
                 <td className="mapkey-table-key">
                   <img src="static/assets/key_files/rail-1b427a2c9a63dd4394db935158f88fb4.png" alt="Rail"/>
                 </td>
                 <td className="mapkey-table-value">
                   Railway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/rail13-825953894f233204a50de1e7fb2db5a1.png" alt="Rail13"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Railway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/subway-e24ad022619a3c87ac3a4f0a8e1900fd.png" alt="Subway"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Subway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/tram-0c6de90fd82d7308a9d3c6163bc4f2d3.png" alt="Tram"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Light rail and tram
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/cable-92a21fd67e8565e947a5de42ba92f218.png" alt="Cable"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Cable car and chairlift
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/runway-2b1883a17a0ecfae5b2317f36094847a.png" alt="Runway"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Airport Runway and taxiway
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/apron-7ba6da34507029a1bc174ecc25ab5611.png" alt="Apron"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Airport apron and terminal
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="0" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/admin-7c4307f7dde215ab019aa034178943e6.png" alt="Admin"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Administrative boundary
                 </td>
               </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="9" data-zoom-max="19">
@@ -189,14 +61,6 @@ module.exports = React.createClass({
               </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
                 <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/golf-bc14891eed710f5213383bf5f945f9c0.png" alt="Golf"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Golf course
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
-                <td className="mapkey-table-key">
                   <img src="static/assets/key_files/park-69cee8ee83faad437693a2f96a19f9eb.png" alt="Park"/>
                 </td>
                 <td className="mapkey-table-value">
@@ -213,50 +77,10 @@ module.exports = React.createClass({
               </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
                 <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/tourist-7f02d5ca0d61ee76130e5ebdbc6555ab.png" alt="Tourist"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Tourist attraction
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
-                <td className="mapkey-table-key">
                   <img src="static/assets/key_files/common-c828f8f45b25952e016974bffa8db41e.png" alt="Common"/>
                 </td>
                 <td className="mapkey-table-value">
                   Common and meadow
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/retail-d00be111a2ed90da1c1fa5916fcf1572.png" alt="Retail"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Retail area
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/industrial-c7b999dbd64ee683211d0b51a9189a33.png" alt="Industrial"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Industrial area
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/commercial-d9dc05aaae2c06b3d3d57f3befa9deb0.png" alt="Commercial"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Commercial area
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="10" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/heathland-5062d2dc5826ab0ebba54aebd8e3b102.png" alt="Heathland"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Heathland
                 </td>
               </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="7" data-zoom-max="19">
@@ -285,130 +109,10 @@ module.exports = React.createClass({
               </tr>
               <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
                 <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/cemetery-9f21b4da8554ea8520cdc11f72de408b.png" alt="Cemetery"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Cemetery
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/allotments-1028203eab70926db14e635e35dd8ba7.png" alt="Allotments"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Allotments
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/pitch-94e1e8537da857c5eb47f8a02435b2f3.png" alt="Pitch"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Sports pitch
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/centre-553dc86764af99635472441a8a2b1dd6.png" alt="Centre"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Sports centre
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
-                <td className="mapkey-table-key">
                   <img src="static/assets/key_files/reserve-d1029f5f3caa44834cfd0d4d873a83ab.png" alt="Reserve"/>
                 </td>
                 <td className="mapkey-table-value">
                   Nature reserve
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="11" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/military-a3e0c59bc64efbc01f2b087c6c555c35.png" alt="Military"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Military area
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/school-7d27465dec7df5c6c51e5eb3b5763069.png" alt="School"/>
-                </td>
-                <td className="mapkey-table-value">
-                  School and university
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/building-0c75f8c3bf00b3963ab9a5e26026c231.png" alt="Building"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Significant building
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/station-6f4a48a24ea408e8ebf90657ef8b128a.png" alt="Station"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Railway station
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/summit-c08127b009dd26bed95b0d7f92ade8cb.png" alt="Summit"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Summit and peak
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/tunnel-d7429a95b68ec88c273e280974487c35.png" alt="Tunnel"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Dashed casing = tunnel
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="13" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/bridge-eb0e326f441bb4c51c36c66e353e4ecd.png" alt="Bridge"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Black casing = bridge
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="15" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/private-193e58175a63102f4b79852ba46e721d.png" alt="Private"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Private access
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="15" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/permissive-84e474d3cfb74366cfd6ae413b0ba91f.png" alt="Permissive"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Permissive access
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="15" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/destination-9d6f0a572ae55fd20a3fd9945f989c85.png" alt="Destination"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Destination access
-                </td>
-              </tr>
-              <tr className="mapkey-table-entry" data-layer="mapnik" data-zoom-min="12" data-zoom-max="19">
-                <td className="mapkey-table-key">
-                  <img src="static/assets/key_files/construction-30ed5373242f19cc27280bb20df2fad7.png" alt="Construction"/>
-                </td>
-                <td className="mapkey-table-value">
-                  Roads under construction
                 </td>
               </tr>
         </tbody>
