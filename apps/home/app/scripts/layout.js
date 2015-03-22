2edc2ea52f1b36f529a0ccb8d6b54a1aade83cae
Richard Hills <richard.hills@10to8.com>
2015-03-22 00:30:06 +0000

    New Key

--- apps/home/app/scripts/layout.js
+++ apps/home/app/scripts/layout.js
@@ -4,6 +4,7 @@ var Reflux = require('reflux');
 
 var ControlBar = require('./controlbar');
 var Map = require('./map');
+var Key = require('./key');
 
 module.exports = React.createClass({
   mixins: [Reflux.ListenerMixin],
@@ -24,10 +25,17 @@ module.exports = React.createClass({
             map_actions={this.props.map_actions}
             map_store={this.props.map_store}/>
         </div>
-        <div className="home-layout__map">
-          <Map
-            map_store={this.props.map_store}
-            map_actions={this.props.map_actions} />
+        <div className="home-layout__contents">
+          <div className="home-layout__map-container">
+            <div className="home-layout__map">
+              <Map
+                map_store={this.props.map_store}
+                map_actions={this.props.map_actions} />
+            </div>
+          </div>
+          <div className="home-layout__key">
+            <Key />
+          </div>
         </div>
       </div>
     );
