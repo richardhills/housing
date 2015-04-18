8caa1f2cf95ceb7043ad25949eca56bef3aa1438
Richard Hills <richard.hills@10to8.com>
2015-04-18 19:16:59 +0100

    Missing word

--- static/js/home.js
+++ static/js/home.js
@@ -605,7 +605,7 @@ module.exports = {
     return "<p><b>Congrats!</b> You have built " + count + " new homes. Can you build " + (28000 - count) + " more?</p>";
   },
   onTargetCompleted: function (count) {
-    return "<p><b>Congratulations!</b><br/><br/>Oxford needed 28000 and you have built " + count + ", enough to last until 2026.</p>";
+    return "<p><b>Congratulations!</b><br/><br/>Oxford needed 28000 homes and you have built " + count + ", enough to last until 2026.</p>";
   },
   sidePanel: (React.createElement("p", null, "Oxford needs 28,000 homes by 2026, and Oxfordshire requires more than 100,000. There is much political disagreement over these numbers (mostly the ", React.createElement("a", { href: "http://www.cpreoxon.org.uk/events/current-events/item/2426-public-meeting-on-over-development-of-oxfordshire" }, "CPRE"), " and some ", React.createElement("a", { href: "http://www.lgcplus.com/opinion/lgc-columnists/in-depth-special-features/why-housing-is-our-priority-in-cash-strapped-times/5075577.article" }, "City Councillors"), "), but one thing is certain; Oxford is desperately short of housing."))
 };
